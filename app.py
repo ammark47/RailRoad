461f23e798debb318753f07dbdfb7dc7a8633ec4
Ammar Karim <ammark47@yahoo.com>
2017-12-24 23:48:04 -0500

    add stations

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask,render_template,request, url_for, redirect
-
+from models.models import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('home.html')
+    available_stations = get_stations()
+    return render_template('home.html', stations = available_stations)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
--- models/models.py
+++ models/models.py
@@ -1,3 +1,11 @@
 import sqlite3
 import datetime
 import datetime as dt
+
+
+def get_stations():
+    db = sqlite3.connect('rrdata.db')
+    cursor = db.cursor()
+    cursor.execute('select * from stations')
+    stations = cursor.fetchall()
+    return stations
